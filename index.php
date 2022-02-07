d25c06bae9435aa27d2f57ca6c3c704e3e20105d
Kozyrev Ilya <kozyrev.dev@gmail.com>
2022-02-07 21:51:54 +0300

    creates the lot price formatting function

--- index.php
+++ index.php
@@ -44,6 +44,20 @@ $lots = [
         'image_url' => 'img/lot-6.jpg'
     ]
 ];
+
+function format_price(float $price = 0): string
+{
+    $formated_price = null;
+    $rounded_price = ceil($price);
+
+    if ($rounded_price < 1000) {
+        $formated_price = $rounded_price;
+    } else {
+        $formated_price = number_format($rounded_price, 0, '', ' ');
+    }
+
+    return $formated_price;
+};
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -130,7 +144,7 @@ $lots = [
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?= $lot['price']; ?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?= format_price($lot['price']); ?><b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
 
