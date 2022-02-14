3fc7a1f52fcaf75ae24749b868c76db345ef903d
Kozyrev Ilya <kozyrev.dev@gmail.com>
2022-02-14 10:51:04 +0300

    adds validation of the login form

--- login.php
+++ login.php
@@ -1,1 +1,39 @@
 <?php
+require 'functions.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $user = $_POST;
+
+    $required = ['email', 'password'];
+
+    $errors = [];
+
+    foreach ($user as $key => $value) {
+        if (in_array($key, $required)) {
+            if ($key == 'email' && !filter_var($value, FILTER_VALIDATE_EMAIL)) $errors[$key] = 'Введите корректный email';
+            if (!$value) $errors[$key] = 'Заполните это поле';
+        }
+    }
+
+    if (count($errors)) {
+        $main_content = include_template('templates/login.php', [
+            'user' => $user,
+            'errors' => $errors
+        ]);
+    } else {
+        header('Location: /index.php');
+    }
+} else {
+    $main_content = include_template('templates/login.php', []);
+}
+
+$layout = include_template('templates/layout.php', [
+    'title' => 'Добавить лот',
+    'is_auth' => (bool) rand(0, 1),
+    'user_name' => 'Константин',
+    'user_avatar' => 'img/user.jpg',
+    'categories' => ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'],
+    'page_content' => $main_content
+]);
+
+print $layout;
--- templates/login.php
+++ templates/login.php
@@ -20,17 +20,32 @@
         </li>
     </ul>
 </nav>
-<form class="form container" action="https://echo.htmlacademy.ru" method="post"> <!-- form--invalid -->
+<?php $class_name = isset($errors) && count($errors) ? 'form--invalid' : ''; ?>
+<form class="form container <?= $class_name; ?>" action="../login.php" method="post"> <!-- form--invalid -->
     <h2>Вход</h2>
-    <div class="form__item"> <!-- form__item--invalid -->
+    <?php
+        $class_name = '';
+        $error_message = '';
+        $value = isset($user['email']) ? $user['email'] : '';
+
+        if (isset($errors['email'])) {
+            $class_name = 'form__item--invalid';
+            $error_message = $errors['email'];
+        }
+    ?>
+    <div class="form__item <?= $class_name; ?>">
         <label for="email">E-mail*</label>
-        <input id="email" type="text" name="email" placeholder="Введите e-mail" required>
-        <span class="form__error">Введите e-mail</span>
+        <input id="email" type="text" name="email" placeholder="Введите e-mail" value='<?= $value; ?>' required>
+        <span class="form__error"><?= $error_message; ?></span>
     </div>
-    <div class="form__item form__item--last">
+    <?php
+        $class_name = isset($errors['password']) ? 'form__item--invalid' : '';
+        $error_message = isset($errors['password']) ? $errors['password'] : '';
+    ?>
+    <div class="form__item form__item--last <?= $class_name; ?>">
         <label for="password">Пароль*</label>
-        <input id="password" type="text" name="password" placeholder="Введите пароль" required>
-        <span class="form__error">Введите пароль</span>
+        <input id="password" type="password" name="password" placeholder="Введите пароль" required>
+        <span class="form__error"><?= $error_message; ?></span>
     </div>
     <button type="submit" class="button">Войти</button>
 </form>
